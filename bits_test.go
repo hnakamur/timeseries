97269531ddc765e05d8d0d1f1e26b8924fb02f6d
Hiroaki Nakamura <hnakamur@gmail.com>
2017-02-12 21:11:58 +0900

    Switch from subtests to normal tests for leading and trailing zeros

--- bits_test.go
+++ bits_test.go
@@ -1,20 +1,15 @@
 package timeseries
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
 func TestNumOfLeadingZeros(t *testing.T) {
 	val := uint64(0x8000000000000000)
 	want := uint8(0)
 	for want <= 64 {
-		t.Run(fmt.Sprintf("%x", val), func(t *testing.T) {
-			got := numOfLeadingZeros(val)
-			if got != want {
-				t.Errorf("got %d; want %d", got, want)
-			}
-		})
+		got := numOfLeadingZeros(val)
+		if got != want {
+			t.Errorf("got %d; want %d", got, want)
+		}
 
 		val >>= 1
 		want++
@@ -25,12 +20,10 @@ func TestNumOfTrailingZeros(t *testing.T) {
 	val := uint64(0x8000000000000000)
 	want := uint8(63)
 	for val > 0 {
-		t.Run(fmt.Sprintf("%x", val), func(t *testing.T) {
-			got := numOfTrailingZeros(val)
-			if got != want {
-				t.Errorf("got %d; want %d", got, want)
-			}
-		})
+		got := numOfTrailingZeros(val)
+		if got != want {
+			t.Errorf("got %d; want %d", got, want)
+		}
 
 		val >>= 1
 		want--
@@ -38,10 +31,8 @@ func TestNumOfTrailingZeros(t *testing.T) {
 
 	val = 0
 	want = 64
-	t.Run("0", func(t *testing.T) {
-		got := numOfTrailingZeros(val)
-		if got != want {
-			t.Errorf("got %d; want %d", got, want)
-		}
-	})
+	got := numOfTrailingZeros(val)
+	if got != want {
+		t.Errorf("got %d; want %d", got, want)
+	}
 }
